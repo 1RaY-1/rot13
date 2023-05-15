7043110ad7dcd5a6de4be8e00953c03198170fb2
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2023-05-15 22:11:52 +0200

    Update rot13.py

--- rot13.py
+++ rot13.py
@@ -1,36 +1,30 @@
 #!/usr/bin/env python3
 
 # ROT13 Кириллическая Версия
-
 import sys
-
-chars_normal = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ " # Отступ не убирать
-chars_rot13 = "лмнопрстуфхцчшщъыьэюяабвгдеёжзийкЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙК " # Отступ не убирать
-
+chars_normal = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ "
+chars_rot13 = "лмнопрстуфхцчшщъыьэюяабвгдеёжзийкЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙК "
 # всякие симболы которые не шивруются
-other_chars = "'\"!@#$%^&*()_+-/.><?|\\}{`~,[]=:;"
-
+other_chars = "'\"!@#$%^&*()_+-/.><?|\\}{`~,[]=:;0123456789"
 action  = ""
 index = []
+_help = f'''
+ROT13 Русскоязычная Версия
 
-_help = f'''При запуске, пишите аргумент "-en" чтобы зашивровать след. текст, или "-de"
-чтобы разшивровать след. текст в ROT-13
+При запуске, пишите аргумент "-en" чтобы зашивровать текст, или "-de" чтобы разшивровать текст используя ROT-13
 
-Пример: python3 {sys.argv[0]} -en "Текст тут"
+Пример: python3 {sys.argv[0]} -en "какой-то текст"
+
 '''
 
 def encode_single(char:str):
     try:
         index = chars_normal.index(char)
         print(chars_rot13[index], end="")
+
     except ValueError:
-#        Если написать число а не букву, то вот так получается, тоесть плюсуем 2
-#       1234567890
-#       3456789012
-        if char.isnumeric() == True:
-            print(int(char) + 2, end="") # " end="" " нужен для того чтобы писать весь текст на одной строке
-#        Если написать какой-то симбол типо ! или ?
-        elif char in other_chars:
+
+        if char in other_chars:
             print(char, end="")
         else:
             sys.exit("Эй, что вы пишите!")
@@ -39,15 +33,12 @@ def decode_single(char:str):
     try:
         index = chars_rot13.index(char)
         print(chars_normal[index], end="")
+
     except ValueError:
-#        А сдесь минусуем 2
-        if char.isnumeric() == True:
-            print(int(char) - 2, end="")
-        elif char in other_chars:
+        if char in other_chars:
             print(char, end="")
         else:
             sys.exit("Эй, что вы пишите!")
-
 def main():
     if action == "encode":
         for i in sys.argv[2]:
@@ -56,13 +47,16 @@ def main():
         for i in sys.argv[2]:
             decode_single(i)
     print()
-
-
 if __name__ == '__main__':
     if len(sys.argv) == 1 or len(sys.argv) == 2:
         sys.exit(_help)
+
     else:
+
         if sys.argv[1] == "-en" or sys.argv[1] == "--encode": action = "encode"
+
         elif sys.argv[1] == "-de" or sys.argv[1] == "--decode": action = "decode"
+
         else: sys.exit("Что-то не так!\n" + _help)
+
         main()
